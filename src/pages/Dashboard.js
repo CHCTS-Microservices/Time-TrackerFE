5ee699c4c7a52f86b1825f4404bfc6826e823a97
Joseph Jia-Han Ho <u6469694@anu.edu.au>
2023-04-29 09:31:30 +1000

    added takt but not completed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import takt from './components/takt';
+import getLocalStorage from './components/get-localstorage';
+import timesheet from './components/timesheet';
+// import React from 'react';
+import React, { useEffect } from 'react';
 import "../App.css"
 
 const Dashboard = () => {
-return (
-	<div         style={{
-        display: 'inline'
-    }}><div
-        style={{
+    useEffect(() => {
+      getLocalStorage();
+      takt();
+      timesheet();
+    }, []);
+
+    return (
+        <div         style={{
             display: 'inline'
-        }}
-    >
-        <h1 className='list1' id='a1'>List palceholder</h1>
-    </div><div
-        style={{
-            display: 'inline'
-        }}
-    >
-            <h1 className='list2' id='a2'>List palceholder2</h1>
-        </div></div>
-    
-);
+        }}><div
+            style={{
+                display: 'inline'
+            }}
+        >
+            <h1 className='list1' id='a1'>List palceholder</h1>
+        </div><div
+            style={{
+                display: 'inline'
+            }}
+        >
+                <h1 className='list2' id='a2'>List palceholder2</h1>
+            </div></div>
+        
+    );
 };
 
 export default Dashboard;
