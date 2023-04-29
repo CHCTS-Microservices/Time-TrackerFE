81f0b750dd6bef56454d55246cdf58c854ac803f
Joseph Jia-Han Ho <u6469694@anu.edu.au>
2023-04-29 10:54:55 +1000

    adding takt-components into src/components folder, Dsahboard is not incomplete

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,35 +1,12 @@
-import takt from './components/takt';
-import getLocalStorage from './components/get-localstorage';
-import timesheet from './components/timesheet';
-// import React from 'react';
-import React, { useEffect } from 'react';
+import React from 'react';
+// import React, { useEffect } from 'react';
 import "../App.css"
+import TimerMain from '../components/TimerMain'
 
 const Dashboard = () => {
-    useEffect(() => {
-      getLocalStorage();
-      takt();
-      timesheet();
-    }, []);
-
     return (
-        <div         style={{
-            display: 'inline'
-        }}><div
-            style={{
-                display: 'inline'
-            }}
-        >
-            <h1 className='list1' id='a1'>List palceholder</h1>
-        </div><div
-            style={{
-                display: 'inline'
-            }}
-        >
-                <h1 className='list2' id='a2'>List palceholder2</h1>
-            </div></div>
-        
+        <h1>AAAA</h1>
     );
-};
-
+    };
+    
 export default Dashboard;
